f85e68df1d1cf84c4ff743520fa7f6fb254655f0
katekut <k_kut2006@inbox.ru>
2025-07-08 22:06:57 +0800

    first function added

---
+++ index.js
@@ -0,0 +1,18 @@
+﻿const TelegramBot = require('node-telegram-bot-api');
+
+const token = '7793508677:AAGO05PY191gZt1_EEDw0V_tTQ1bDsQzGY0';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Привет, Октагон!");
+});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log("Получено сообщение от : ");
+});
+
+console.log("Бот запущен!");
